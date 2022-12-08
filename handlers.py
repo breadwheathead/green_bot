3a43da621b38b86b086323f7c00a9c72154f5069
Bread <breadwheatbox@gmail.com>
2022-12-08 20:07:16 +0700

    check admin access completed

--- handlers.py
+++ handlers.py
@@ -1,18 +1,24 @@
-from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
+from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery, ContentTypes
 from aiogram.dispatcher import Dispatcher, filters
 from bot_init import bot
+from config import ADMIN_ID
 from messages import MESSAGES
 from keyboards import reply_button, keyboard
 
 
-async def callback_weather(callback_query: CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Тут потом будет погода')
+async def callback_weather(call: CallbackQuery):
+    await bot.answer_callback_query(call.id)
+    await bot.send_message(call.from_user.id, 'Тут потом будет погода')
+    await call.answer(text='Спасибо, что воспользовались ботом!', show_alert=True)
 
 
-async def callback_rate(callback_query: CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Тут потом будет курс доллара')
+async def callback_rate(call: CallbackQuery):
+    await bot.answer_callback_query(call.id)
+    await bot.send_message(call.from_user.id, 'Тут потом будет курс доллара')
+
+
+async def access_denied(message: Message):
+    await message.answer('Вам отказано в доступе!')
 
 
 async def start_command(message: Message):
@@ -36,11 +42,21 @@ async def show_keyboard(message: Message):
     await bot.send_message(message.from_user.id, 'Вот что я могу 😊', reply_markup=keyboard)
 
 
+async def unknown_command(message: Message):
+    await message.delete()
+
+
 def register_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(callback_weather, lambda c: c.data == 'weather')
+    dp.register_callback_query_handler(callback_rate, lambda c: c.data == 'rate')
+
+    '''access denied'''
+    dp.register_message_handler(access_denied, lambda m: m.chat.id != ADMIN_ID, content_types=ContentTypes.ANY)
+
     dp.register_message_handler(start_command, commands=['start', 'help'])
     # dp.register_message_handler(help_command, commands='help')
     # dp.register_message_handler(weather_command, commands='weather')
     # dp.register_message_handler(rate_command, commands='rate')
-    dp.register_message_handler(show_keyboard, filters.Text(equals='Показать кнопки'))
-    dp.register_callback_query_handler(callback_weather, lambda c: c.data == 'weather')
-    dp.register_callback_query_handler(callback_rate, lambda c: c.data == 'rate')
+    dp.register_message_handler(show_keyboard, filters.IDFilter(ADMIN_ID), filters.Text(equals='Показать кнопки'))
+    dp.register_message_handler(unknown_command, filters.IDFilter(ADMIN_ID), content_types=ContentTypes.ANY)
+
