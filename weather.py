d87cb7fb67bea312ff1129de938a02f4a724dac3
Bread <breadwheatbox@gmail.com>
2022-12-09 11:38:14 +0700

    gismeteo invalid token

--- weather.py
+++ weather.py
@@ -1,9 +1,31 @@
 import requests
-from config import OPEN_WEATHER_API_KEY
+from pprint import pprint
+from config import X_GISMETEO_TOKEN
 
-URL = f'https://api.openweathermap.org/data/3.0/onecall?' \
-      f'lat={lat}&lon={lon}&exclude=current&appid={OPEN_WEATHER_API_KEY}&units=metric&land=ru'
+URL = 'https://api.gismeteo.net/v2/weather/current/4368/'
+
 
 def get_weather(city):
+    headers = {
+        'X-Gismeteo-Token': '56b30cb255.3443075'
+    }
+    session = requests.Session()
+    session.headers.update(headers)
     try:
-        r = requests.get()
+        r = session.get(URL)
+        print(session.headers)
+        print(r.headers)
+        data = r.json()
+        pprint(data)
+    except Exception as e:
+        print(e)
+        print('Ошибка')
+
+
+def main():
+    city = 'Moscow'
+    get_weather(city)
+
+
+if __name__ == '__main__':
+    main()
